b6896c8dc028e4100af55f37f786a4b5d988b409
Rinku Kazeno <development@kazeno.co>
2014-05-17 10:01:36 +0100

    0.4 - config interface done

--- classes/OcaEpakOperative.php
+++ classes/OcaEpakOperative.php
@@ -5,22 +5,42 @@
 
 class OcaEpakOperative extends ObjectModel
 {
-    public $id;
+    //public $id;
+    public $reference;
     public $description;
     public $addfee;
+    public $id_shop;
 
     /**
      * @see ObjectModel::$definition
      */
     public static $definition = array(
         'table' => OcaEpak::OPERATIVES_TABLE,
-        'primary' => 'id',
+        'primary' => OcaEpak::OPERATIVES_ID,
         'multishop' => TRUE,
         'fields' => array(
-            'id' =>	array('type' => self::TYPE_INT, 'validate' => 'isunsignedInt', 'required' => TRUE),
+            'reference' =>	array('type' => self::TYPE_INT, 'validate' => 'isunsignedInt', 'required' => TRUE),
             'description' => array('type' => self::TYPE_STRING, 'validate' => 'isGenericName', 'required' => TRUE),
             'addfee' => array('type' => self::TYPE_STRING, 'validate' => 'isString', 'required' => FALSE),
             'id_shop' => array('type' => self::TYPE_INT, 'validate' => 'isunsignedInt', 'required' => true),
         )
     );
+
+    public function validateFields($die = true, $error_return = false)
+    {
+        $message = parent::validateFields($die, TRUE);
+        if ($message !== TRUE)
+            return $error_return ? $message : false;
+        $message = (!preg_match('/^[\d]*[\.]?[\d]*%?$/', $this->addfee) OR $this->addfee == '%')
+            ? Translate::getModuleTranslation('OcaEpak','Optional fee format is incorrect. Should be either an amount, such as 7.50, or a percentage, such as 6.99%','OcaEpak')
+        : TRUE;
+        if ($message !== true)
+        {
+            if ($die)
+                throw new PrestaShopException($message);
+            return $error_return ? $message : false;
+        }
+
+        return TRUE;
+    }
 }
